9c401355b71e0798ee22612364b4e032cb819670
Pavol Havlik <pavolhavlik28@gmail.com>
2023-09-27 10:27:35 +0200

    Replacing deprecated bodyparser

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import http from 'http'
 import express from 'express'
-import * as bodyParser from 'body-parser'
+// import * as bodyParser from 'body-parser'
 
 import { sequelize } from './db'
 import ProgramRouter from './routes/programs'
@@ -8,17 +8,30 @@ import ExerciseRouter from './routes/exercises'
 
 const app = express()
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+// Original parsing
+// app.use(bodyParser.urlencoded({ extended: true }))
+// app.use(bodyParser.json())
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/programs', ProgramRouter())
 app.use('/exercises', ExerciseRouter())
 
-const httpServer = http.createServer(app)
+// const httpServer = http.createServer(app)
 
-sequelize.sync()
+// sequelize.sync()
 
-console.log('Sync database', 'postgresql://localhost:5432/fitness_app')
+// console.log('Sync database', 'postgresql://localhost:5432/fitness_app')
 
-httpServer.listen(8000).on('listening', () => console.log(`Server started at port ${8000}`))
+// httpServer.listen(8000).on('listening', () => console.log(`Server started at port ${8000}`))
 
-export default httpServer
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
+
+// export default httpServer
