040c0dd7da310de9d41b2e32570ffddc40da3bdd
Pavol Havlik <pavolhavlik28@gmail.com>
2023-09-27 11:44:32 +0200

    Replaced double quotes with single quotes

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,9 +1,9 @@
 export const dbConfig = {
-    HOST: "localhost",
-    USER: "pavolh",
-    PASSWORD: "pavol123",
-    DB: "fitness_app",
-    dialect: "postgres",
+    HOST: 'localhost',
+    USER: 'pavolh',
+    PASSWORD: 'pavol123',
+    DB: 'fitness_app',
+    dialect: 'postgres',
     pool: {
         max: 5,
         min: 0,
