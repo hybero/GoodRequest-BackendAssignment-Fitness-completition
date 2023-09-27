a273a82fef89625af47d6ec8cb66363d22773068
Pavol Havlik <pavolhavlik28@gmail.com>
2023-09-27 10:29:18 +0200

    Added db.config.ts

---
+++ config/db.config.ts
@@ -0,0 +1,13 @@
+export const dbConfig = {
+    HOST: "localhost",
+    USER: "postgres",
+    PASSWORD: "pavol123",
+    DB: "fitness_app",
+    dialect: "postgres",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
