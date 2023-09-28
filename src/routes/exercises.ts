baed1edfbf8fbf4d94dc359644f95ec4a8f0d529
Pavol Havlik <pavolhavlik28@gmail.com>
2023-09-28 16:50:42 +0200

    Added Exercise routes post, put and delete

--- src/routes/exercises.ts
+++ src/routes/exercises.ts
@@ -2,6 +2,8 @@ import { Router, Request, Response, NextFunction } from 'express'
 import { verifyRoles } from '../middleware/verifyRoles'
 
 import { models } from '../db'
+import { ExerciseModel } from '../db/exercise'
+import { EXERCISE_DIFFICULTY } from '../utils/enums'
 
 const router: Router = Router()
 
@@ -10,7 +12,7 @@ const {
 	Program
 } = models
 
-router.get('/', verifyRoles('ADMIN'), async (_req: Request, res: Response, _next: NextFunction) => {
+router.get('/admin', verifyRoles('ADMIN'), async (req: Request, res: Response) => {
 	
 	const exercises = await Exercise.findAll({
 		include: [{
@@ -25,7 +27,65 @@ router.get('/', verifyRoles('ADMIN'), async (_req: Request, res: Response, _next
 	})
 })
 
-router.get('/', verifyRoles('ADMIN'), async (_req: Request, res: Response, _next: NextFunction) => {
+router.post('/admin', verifyRoles('ADMIN'), async (req: Request, res: Response) => {
+
+	if(!req.body.difficulty || !req.body.name || !req.body.programID) return res.status(400).json({ 'message': 'Difficulty, name and programID are required.' })
+
+	if(!Object.values(EXERCISE_DIFFICULTY).includes(req.body.difficulty)) return res.status(400).json({ 'message': 'Difficulty value is invalid.' })
+
+	const { difficulty, name, programID } = req.body
+
+	const exercise = await ExerciseModel.create({
+		difficulty: difficulty,
+		name: name,
+		programID: programID || null
+	})
+	
+	return res.json({
+		data: exercise,
+		message: 'Exercise created'
+	})
+
+})
+
+router.put('/admin/:id?', verifyRoles('ADMIN'), async (req: Request, res: Response) => {
+
+	if(!req.params.id) return res.status(400).json({ 'message': 'Parameter id is required.' })
+
+	if(!Object.values(EXERCISE_DIFFICULTY).includes(req.body.difficulty)) return res.status(400).json({ 'message': 'Difficulty value is invalid.' })
+
+	let exercise = await ExerciseModel.findOne({
+		where: { id: req.params.id }
+	})
+
+	if('difficulty' in req.body) exercise.difficulty = req.body.difficulty
+	if('name' in req.body) exercise.name = req.body.name
+	if('programID' in req.body) exercise.programID = req.body.programID
+
+	exercise.save()
+	
+	return res.json({
+		data: exercise,
+		message: 'Exercise updated'
+	})
+
+})
+
+router.delete('/admin/:id?', verifyRoles('ADMIN'), async (req: Request, res: Response) => {
+
+	if(!req.params.id) return res.status(400).json({ 'message': 'Parameter id is required.' })
+
+	let exercise = await ExerciseModel.findOne({
+		where: { id: req.params.id }
+	})
+
+	if(!exercise) return res.status(204).json({ 'message': `Exercise with id ${req.params.id} not found.` })
+
+	await ExerciseModel.destroy({
+		where: { id: req.params.id }
+	  });
+	
+	return res.json({ 'message': 'Exercise deleted.' })
 
 })
 
