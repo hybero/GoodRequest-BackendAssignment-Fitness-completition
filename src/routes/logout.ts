2ad5f228b588b627b94bb273eb83f850b68c1edd
Pavol Havlik <pavolhavlik28@gmail.com>
2023-09-30 10:00:32 +0200

    Added response message when no cookie with refresh token was sent

--- src/routes/logout.ts
+++ src/routes/logout.ts
@@ -8,7 +8,7 @@ router.get('/', async (req: Request, res: Response) => {
     
     const cookies = req.cookies
 
-    if(!cookies?.jwt) return res.sendStatus(204)
+    if(!cookies?.jwt) return res.sendStatus(204).json({ 'message': 'Missing cookie, nothing to do.' })
     
     const refreshToken = cookies.jwt
     
