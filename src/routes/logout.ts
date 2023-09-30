a30ae500023cb308a06f7aa344e2b0d009f880fa
Pavol Havlik <pavolhavlik28@gmail.com>
2023-09-30 10:01:52 +0200

    Changed response status when missing cookie with refresh token

--- src/routes/logout.ts
+++ src/routes/logout.ts
@@ -8,7 +8,7 @@ router.get('/', async (req: Request, res: Response) => {
     
     const cookies = req.cookies
 
-    if(!cookies?.jwt) return res.sendStatus(204).json({ 'message': 'Missing cookie, nothing to do.' })
+    if(!cookies?.jwt) return res.sendStatus(404).json({ 'message': 'Missing cookie, nothing to do.' })
     
     const refreshToken = cookies.jwt
     
