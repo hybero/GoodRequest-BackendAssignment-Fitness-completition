b85c8184def50bc055f7e052b93c047d46f1e1fe
Pavol Havlik <pavolhavlik28@gmail.com>
2023-10-01 10:15:35 +0200

    Added option for localization to be in upper case in the request headers

--- src/middleware/localization.ts
+++ src/middleware/localization.ts
@@ -4,8 +4,10 @@ import { Request, Response, NextFunction } from 'express'
 export const localizationHeaders = (req: Request, res: Response, next: NextFunction) => {
     
     const localizationHeader: string = String(req.headers.localization) || 'en'
+    const lowerCaseLocalizationHeader = localizationHeader.toLowerCase()
+    req.headers.localization = lowerCaseLocalizationHeader
 
-    if(!['sk', 'en'].includes(localizationHeader)) {
+    if(!['sk', 'en'].includes(lowerCaseLocalizationHeader)) {
         req.headers.localization = 'en'
     }
 
